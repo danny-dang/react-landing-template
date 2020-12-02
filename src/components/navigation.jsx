b3cb745fcf4ec3c30cf18b18ee4e322f1cb4b924
Nguyen Phi Dang <dangnp.bhsoft@gmail.com>
2020-12-02 14:09:35 +0700

    rename

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -19,7 +19,7 @@ export class Navigation extends Component {
               <span className="icon-bar"></span>{" "}
             </button>
             <a className="navbar-brand page-scroll" href="#page-top">
-              React Landing Page
+              Juntos Network
             </a>{" "}
           </div>
 
@@ -41,21 +41,6 @@ export class Navigation extends Component {
               <li>
                 <a href="#services" className="page-scroll">
                   Services
-                </a>
-              </li>
-              <li>
-                <a href="#portfolio" className="page-scroll">
-                  Gallery
-                </a>
-              </li>
-              <li>
-                <a href="#testimonials" className="page-scroll">
-                  Testimonials
-                </a>
-              </li>
-              <li>
-                <a href="#team" className="page-scroll">
-                  Team
                 </a>
               </li>
               <li>
